f67f523fb709f014e1bbd2b2624aad326026b1ae
Colin Spear <colinspear@gmail.com>
2021-01-29 16:45:23 -0800

    use from_envar in config file

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 from flask import Flask
 
-print(os.environ['APP_SETTINGS'])
+# print(os.environ['APP_SETTINGS'])
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_envvar('APP_SETTINGS')
 
 
 @app.route('/')
