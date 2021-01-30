00485cc63f63ca82ccde73432261bc247a80a8a8
Colin Spear <colinspear@gmail.com>
2021-01-29 16:51:51 -0800

    drop config.py from gitignore

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'pick-a-new-key-man'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = False
+
+
+class TestingConfig(Config):
+    TESTING = True
