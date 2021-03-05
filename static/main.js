217377ee09b9b5932fd27e6391256dbe0c88d1c8
Colin Spear <colinspear@gmail.com>
2021-03-05 09:32:47 -0800

    finished app

--- static/main.js
+++ static/main.js
@@ -6,26 +6,32 @@
     .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
   
     function($scope, $log, $http, $timeout) {
-  
-    $scope.getResults = function() {
-    
-        $log.log("test");
-    
-        // get the URL from the input
-        var userInput = $scope.url;
-    
-        // fire the API request
-        $http.post('/start', {"url": userInput}).
-        success(function(results) {
-            $log.log(results);
-            getWordCount(results);
-
-        }).
-        error(function(error) {
-            $log.log(error);
-        });
-    
-    };
+        
+        $scope.submitButtonText = 'Submit';
+        $scope.loading = false;
+        $scope.urlerror = false;
+        $scope.getResults = function() {
+        
+            $log.log("test");
+        
+            // get the URL from the input
+            var userInput = $scope.url;
+        
+            // fire the API request
+            $http.post('/start', {"url": userInput}).
+            success(function(results) {
+                $log.log(results);
+                getWordCount(results);
+                $scope.wordcounts = null;
+                $scope.loading = true;
+                $scope.submitButtonText = 'Loading...';
+                $scope.urlerror = false;
+            }).
+            error(function(error) {
+                $log.log(error);
+            });
+        
+        };
 
     function getWordCount(jobID) {
         var timeout = "";
@@ -38,20 +44,56 @@
                         $log.log(data, status);
                     } else if (status == 200){
                         $log.log(data);
-                        $scope.wordcounts = data
+                        $scope.loading = false;
+                        $scope.submitButtonText = 'Submit';
+                        $scope.wordcounts = data;   
                         $timeout.cancel(timeout);
                         return false;
                     }
                     // conitinue calling poller ufnction every 2 seconds
                     // until timeout is canelled
                     timeout = $timeout(poller, 2000);
+                }).
+                error(function(error) {
+                    $log.log(error);
+                    $scope.loading = false;
+                    $scope.submitButtonText = "Submit";
+                    $scope.urlerror = true;
                 });
         };
         poller();
     };
-  
-  }
-  ]);  
+    }
+    ])
+    .directive('wordCountChart', ['$parse', function ($parse) {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div id="chart"></div>',
+            link: function (scope) {
+                scope.$watch('wordcounts', function() {
+                    d3.select('#chart').selectAll('*').remove();
+                    var data = scope.wordcounts;
+                    for (var word in data) {
+                        var key = data[word][0];
+                        var value = data[word][1];
+                        d3.select('#chart')
+                            .append('div')
+                            .selectAll('div')
+                            .data(word)
+                            .enter()
+                            .append('div')
+                            .style('width', function() {
+                                return (value * 3) + 'px';
+                            })
+                            .text(function(d){
+                                return key;
+                            });
+                    }
+                }, true)
+            }
+        };
+    }]);  
   
   }());
   
